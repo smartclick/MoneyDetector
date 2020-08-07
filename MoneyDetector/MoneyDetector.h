af8cee3503396b752483bdd336117e839fff82b1
Sevak Soghoyan <sevak.soghoyan@smartclick.ai>
2020-08-07 11:11:49 +0400

    Initial Commit

---
+++ MoneyDetector/MoneyDetector.h
@@ -0,0 +1,19 @@
+//
+//  MoneyDetector.h
+//  MoneyDetector
+//
+//  Created by Sevak Soghoyan on 8/7/20.
+//  Copyright © 2020 Sevak Soghoyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MoneyDetector.
+FOUNDATION_EXPORT double MoneyDetectorVersionNumber;
+
+//! Project version string for MoneyDetector.
+FOUNDATION_EXPORT const unsigned char MoneyDetectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoneyDetector/PublicHeader.h>
+
+
